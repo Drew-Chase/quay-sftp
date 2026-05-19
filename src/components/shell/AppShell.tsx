245299a41caa4387333d87a7d3d6f141aeeed097
Drew Chase (Work) <dcmanproductions@gmail.com>
2026-05-19 14:03:35 -0400

    Add ErrorBoundary component and wrap Outlet in AppShell

--- src/components/shell/AppShell.tsx
+++ src/components/shell/AppShell.tsx
@@ -1,5 +1,6 @@
 import {Outlet} from "react-router-dom";
 import WindowChrome from "./WindowChrome";
+import {ErrorBoundary} from "../../ErrorBoundry.tsx";
 
 // Layout for all in-app routes: titlebar on top, sidebar on left, page content on right.
 export default function AppShell()
@@ -9,7 +10,9 @@ export default function AppShell()
             <WindowChrome/>
             <div className="flex flex-row flex-1 min-h-0">
                 <div className="flex flex-col flex-1 min-w-0 min-h-0">
-                    <Outlet/>
+                    <ErrorBoundary>
+                        <Outlet/>
+                    </ErrorBoundary>
                 </div>
             </div>
         </div>
