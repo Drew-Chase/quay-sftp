5797688a34558ce00a6aaea1bf184a50293ce772
Drew Chase <drew.chase@mardens.com>
2026-06-12 15:58:25 -0400

    Define QuayConnector trait and QuayError types in quay_lib

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -1,18 +1,45 @@
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+use crate::ftp::FTPConnection;
+
 pub mod ftp;
 pub mod sftp;
 pub mod s3;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(thiserror::Error, Debug)]
+pub enum QuayError {
+    #[error("Enumeration error: {0}")]
+    EnumerationError(#[from] std::io::Error),
+    #[error("Failed to upload {0} bytes to remote path '{1}': {2}")]
+    UploadError(usize, String, std::io::Error),
+    #[error("{0}")]
+    IoError(std::io::Error),
+    #[error("Lock poisoned")]
+    LockError,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+static CURRENT_CONNECTION: Arc<Mutex<Option<impl QuayConnector>>> = Arc::new(Mutex::new(None));
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+trait QuayConnector {
+    fn list(&self) -> Result<Vec<String>, QuayError>;
+    fn upload(&self, bytes: &[u8], remote_path: &str)->Result<(), QuayError>;
+    fn get_bytes(&self, remote_path: &str)->Result<Vec<u8>, QuayError>;
+    fn connect(&self)->Result<(), QuayError>;
+    fn download(&self, host_path: impl Into<PathBuf>, remote_path: &str)->Result<(), QuayError>{
+        let bytes = self.get_bytes(remote_path)?;
+        std::fs::write(host_path, bytes)?;
+        Ok(())
+    }
+    
+    fn unmount(&self)->Result<(), QuayError>{
+        *CURRENT_CONNECTION.lock()? = None;
+        Ok(())
+    }
+    fn mount(&self)->Result<(),QuayError>{
+        *CURRENT_CONNECTION.lock()? = Some(FTPConnection{});
+        Ok(())
+    }
+    fn current()->Option<Arc<Mutex<Self>>>{
+        CURRENT_CONNECTION.lock().unwrap().clone()
     }
 }
