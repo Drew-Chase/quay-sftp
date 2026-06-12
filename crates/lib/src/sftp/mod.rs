bc080a1fdc41eade287a9b5073f5c355c3db017f
Drew Chase <drew.chase@mardens.com>
2026-06-12 15:58:28 -0400

    Implement QuayConnector stubs for FTP, SFTP, and S3 protocols

---
+++ crates/lib/src/sftp/mod.rs
@@ -0,0 +1,22 @@
+use crate::{QuayConnector, QuayError};
+
+pub struct SFTP;
+
+impl QuayConnector for SFTP {
+	fn list(&self) -> Result<Vec<String>, QuayError> {
+		todo!()
+	}
+
+	fn upload(&self, bytes: &[u8], remote_path: &str) -> Result<(), QuayError> {
+		todo!()
+	}
+
+	fn get_bytes(&self, remote_path: &str) -> Result<Vec<u8>, QuayError> {
+		todo!()
+	}
+
+	fn connect(&self) -> Result<(), QuayError> {
+		// TODO: Handle connection setup
+		self.mount()
+	}
+}
